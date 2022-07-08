9c9084bf173347bc4ba171ed2dfc4d41d8bf1beb
Kentaro <kentaro@beah.jp>
2022-07-09 08:19:49 +0900

    add: 認証情報登録View

---
+++ watchTOTP/CredentialAddView.swift
@@ -0,0 +1,20 @@
+//
+//  CredentialAddView.swift
+//  watchTOTP
+//
+//  Created by 阿部 賢太郎 on 2022/07/09.
+//
+
+import SwiftUI
+
+struct CredentialAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CredentialAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        CredentialAddView()
+    }
+}
